70346e86d3bc4627f35df41af533f08453cf46f6
Alex Chan <alex@silverprogram.com>
2014-07-12 14:55:09 -0700

    Added ssh user

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  SSH_USERPASS=`pwgen -c -n -1 8`
+  mkdir /home/user
+  useradd -G sudo -d /home/user user
+  chown user /home/user
+  echo user:$SSH_USERPASS | chpasswd
+  echo ssh user password: $SSH_USERPASS
+
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
   /usr/bin/mysqld_safe & 
   sleep 10s
